847d97789019a345220e87533db74d7175811300
Sydney <proteimus@gmail.com>
2024-08-08 20:33:22 -0500

    server w/EXPRESS

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 // IMPORTS AT THE TOP
+const express = require('express')
 
 // INSTANCE OF EXPRESS APP
+const server = express()
 
 // GLOBAL MIDDLEWARE
+server.use(express.json()) 
+
 
 // ENDPOINTS
 
@@ -13,4 +17,5 @@
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
-// EXPOSING THE SERVER TO OTHER MODULES
+// EXPOSING THE SERVER TO OTHER MODULES//////
+module.exports = server
