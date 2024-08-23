0a3142e4c79f5cb42c812bc6d314e0478163debe
Sydney <proteimus@gmail.com>
2024-08-23 09:56:00 -0500

    sending APIs

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // IMPORTS AT THE TOP
 const express = require('express')
+const Dog = require('./dog-model') 
 
 // INSTANCE OF EXPRESS APP
 const server = express()
@@ -9,9 +10,21 @@ server.use(express.json())
 
 
 // ENDPOINTS
-
 // [GET]    /             (Hello World endpoint)
+
+server.get('/hello-world', (req, res) => {
+  res.status(200).json({ message: "hello, world" })
+}) 
+
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
+server.get('/api/dogs', async (req, res) => {
+  try {
+    const dogs = await Dog.findAll() 
+    console.log(dogs);
+  } catch(err) {
+    res.status(500).json({ message: `Something horrible: ${err.message}`})
+  }
+})
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
